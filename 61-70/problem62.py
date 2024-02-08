3843c3c880b59007fe55749b007b537ec43215f8
James Weatherill <weatherill.james@icloud.com>
2024-02-08 13:44:56 +0000

    problem62

---
+++ 61-70/problem62.py
@@ -0,0 +1,39 @@
+#! /opt/homebrew/bin/python3
+
+########################################
+
+from time import time
+
+########################################
+
+startTime = time()
+
+allNums = {}
+
+counter = 1
+
+while True:
+    counterCube = counter**3
+    sortedCounterCube = "".join(sorted(list(str(counterCube))))
+
+    if sortedCounterCube in allNums:
+        allNums[sortedCounterCube][0] += 1
+        allNums[sortedCounterCube][1].append(counterCube)
+    else:
+        allNums[sortedCounterCube] = [1, [counterCube]]
+
+    if allNums[sortedCounterCube][0] == 5:
+        break
+
+    counter += 1
+
+isolatedList = allNums[sortedCounterCube][1]
+finalStr = "\n".join([f"{i+1}) {str(isolatedList[i])}" for i in range(len(isolatedList))])
+
+print(f"\nAll possible permutations:\n\n{finalStr}")
+
+finishTime = time()
+
+########################################
+
+print(f"\nThe code took: {finishTime-startTime} seconds, to run!\n")
