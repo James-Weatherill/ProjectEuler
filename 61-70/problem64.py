f8913ec8476dd5bcb531f05ef5a62b4b4133f629
James Weatherill <weatherill.james@icloud.com>
2024-02-19 22:15:17 +0000

    Finished problem64

--- 61-70/problem64.py
+++ 61-70/problem64.py
@@ -3,54 +3,47 @@
 ########################################
 
 from time import time
-from math import sqrt
+from mpmath import mp
 
 ########################################
 
 startTime = time()
 
+mp.dps = 400
+
 ans = 0
 
 for num in range(2, 10_001):
 
-    if sqrt(num)%1!=0:
+    if mp.sqrt(num)%1!=0:
 
         tempList = []
 
-        rootOfNum = sqrt(num)
+        rootOfNum = mp.sqrt(num)
         leadingDigit = int(rootOfNum)
 
         fraction = 1/(rootOfNum-leadingDigit)
 
-        for i in range(10):
+        for i in range(300):
             leadingDigitOfFrac = int(fraction)
             tempList.append(leadingDigitOfFrac)
             fraction -= leadingDigitOfFrac
-            fraction = fraction**(-1)
+            fraction = mp.power(fraction, (-1))
 
-        five1 = tempList[0:5]
-        five2 = tempList[5:10]
-        three1 = tempList[0:3]
-        three2 = tempList[3:6]
-        three3 = tempList[6:9]
-        one1 = tempList[0]
-        one2 = tempList[1]
-        one3 = tempList[2]
-        one4 = tempList[3]
-        one5 = tempList[4]
-        one6 = tempList[5]
-        one7 = tempList[6]
-        one8 = tempList[7]
-        one9 = tempList[8]
-        one10 = tempList[9]
+        if len(tempList)%2 != 0:
+            tempList = tempList[0:-1]
 
-        if five1 == five2:
-            ans += 1
-        elif three1 == three2 and three2 == three3:
-            ans +=1
-        elif one1 == one2 and one2 == one3 and one3 == one4 and one4 == one5 and one5 == one6 and one6 == one7 and one7 == one8 and one8 == one9 and one9 == one10:
-            ans += 1
+        while len(tempList)%2 == 0 and tempList[0:len(tempList)//2] != tempList[len(tempList)//2:len(tempList)]:
+            tempList = tempList[0:-2]
 
+        if len(tempList) != 0:
+
+            while len(tempList)%2 == 0 and tempList[0:len(tempList)//2] == tempList[len(tempList)//2:len(tempList)]:
+                tempList = tempList[0:len(tempList)//2]
+
+            if len(tempList)%2 != 0:
+                ans += 1
+        
 print(ans)
 
 finishTime = time()
