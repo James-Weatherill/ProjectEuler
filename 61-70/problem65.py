52e3f0f262f16301469942656f1659e0aa455783
James Weatherill <weatherill.james@icloud.com>
2024-02-27 16:44:08 +0000

    finished problem 65

--- 61-70/problem65.py
+++ 61-70/problem65.py
@@ -3,21 +3,25 @@
 ########################################
 
 from time import time
-from mpmath import mp
 
 ########################################
 
 startTime = time()
 
-mp.dps = 100
-
 eList = []
 counter = 1
-for i in range(3):
+for i in range(33):
     eList = eList + [1, counter * 2, 1]
     counter += 1
 
-print(eList)
+ansList = [[1, eList[-1]]]
+
+for i in range(len(eList)-2, -1, -1):
+    ansList.append([ansList[-1][1], eList[i] * ansList[-1][1] + ansList[-1][0]])
+
+ans = sum(int(x) for x in list(str(ansList[-1][1] * 2 + ansList[-1][0])))
+
+print(ans)
 
 finishTime = time()
 
